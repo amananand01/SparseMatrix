83af5426381be61c6f574119bbb0aee7df26ed0b
lemonpopcorn <lemonpopcorn>
2019-02-03 16:08:05 -0700

    Add pre- and post-conditions for simple operations.

--- SparseMatrix.rb
+++ SparseMatrix.rb
@@ -2,12 +2,25 @@ require 'test/unit'
 
 module SparseMatrix
 
+  include Test::Unit::Assertions
+
   class Shape
     attr_accessor :m
     attr_accessor :n
     def initialize(m, n)
+      # Pre:
+      assert(m > 0 && n > 0, "Negative shapes are not allowed.")
+
       @m = m
       @n = n
+
+      # Post:
+      assert(self.m == m && self.n == n)
+    end
+
+    def ==(other)
+      # Pre:
+      assert(other.is_a?(Shape))
     end
   end
 
@@ -53,12 +66,140 @@ module SparseMatrix
       @shape = shape
     end
 
+    def [](m, n)
+      # Pre:
+      assert(0 <= m < @shape.m)
+      assert(0 <= n < @shape.n)
+    end
+
+    def []=(m, n, v)
+      # Pre:
+      assert(0 <= m < @shape.m)
+      assert(0 <= n < @shape.n)
+
+      # Post:
+      assert(self[m,n] == v)
+    end
+
+    def transpose()
+      # Pre:
+      old = self.clone
+      
+      # Post:
+      for i in old.shape.m
+        for j in old.shape.n
+          assert(old[i,j] == self[j,i])
+        end
+      end
+    end
+
+    def add(other)
+      # Pre:
+      other.is_a?(AbstractMatrix)
+      other.shape == @shape
+      old = self.clone
+
+      # Post:
+      for i in old.shape.m
+        for j in old.shape.n
+          assert(self[i,j] == old[i,j] + other[i,j])
+        end
+      end
+    end
+
+    def subtract(other)
+      # Pre:
+      other.is_a?(AbstractMatrix)
+      other.shape == @shape
+      old = self.clone
+
+      # Post:
+      for i in old.shape.m
+        for j in old.shape.n
+          assert(self[i,j] == old[i,j] - other[i,j])
+        end
+      end
+    end
+
+    def multiply(other)
+    end
+
+    def determinant()
+    end
+
+    def inverse()
+    end
+
   end
 
   class TridiagonalSparseMatrix < AbstractMatrix
     attr_accessor :shape
     def initialize(shape)
       @shape = shape
+    end
+
+    def [](m, n)
+      # Pre:
+      assert(0 <= m < @shape.m)
+      assert(0 <= n < @shape.n)
+    end
+
+    def []=(m, n, v)
+      # Pre:
+      assert(0 <= m < @shape.m)
+      assert(0 <= n < @shape.n)
+
+      # Post:
+      assert(self[m,n] == v)
+    end
+
+    def transpose()
+      # Pre:
+      old = self.clone
+      
+      # Post:
+      for i in old.shape.m
+        for j in old.shape.n
+          assert(old[i,j] == self[j,i])
+        end
+      end
+    end
+
+    def add(other)
+      # Pre:
+      other.is_a?(AbstractMatrix)
+      other.shape == @shape
+      old = self.clone
+
+      # Post:
+      for i in old.shape.m
+        for j in old.shape.n
+          assert(self[i,j] == old[i,j] + other[i,j])
+        end
+      end
+    end
+
+    def subtract(other)
+      # Pre:
+      other.is_a?(AbstractMatrix)
+      other.shape == @shape
+      old = self.clone
+
+      # Post:
+      for i in old.shape.m
+        for j in old.shape.n
+          assert(self[i,j] == old[i,j] - other[i,j])
+        end
+      end
+    end
+
+    def multiply(other)
+    end
+
+    def determinant()
+    end
+
+    def inverse()
     end
 
   end
